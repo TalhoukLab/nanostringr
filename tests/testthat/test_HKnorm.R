0981099f3d5be214818fae7099a64fc40726e3ec
Derek Chiu <dchiu@bccrc.ca>
2016-09-08 11:03:14 -0700

    unnecessary test

--- tests/testthat/test_HKnorm.R
+++ tests/testthat/test_HKnorm.R
@@ -1,9 +1,5 @@
 context("HKnorm")
 
-test_that("Error if no endogenous and housekeeping genes", {
-  expect_error(HKnorm(NanoString.mRNA))
-})
-
 NanoString.mRNA[NanoString.mRNA$Name %in%
 c('Eef1a1','Gapdh','Hprt1','Ppia','Sdha'), 'Code.Class'] <- 'Housekeeping'
 
