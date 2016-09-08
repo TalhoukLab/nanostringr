c16c48db0ea964691ffd7e25e93e76d286425d68
Derek Chiu <dchiu@bccrc.ca>
2016-09-08 11:02:33 -0700

    better param docs, functionalized egs, handle empty subtitles

--- R/CCplot.R
+++ R/CCplot.R
@@ -5,9 +5,9 @@
 #' @param method1 measurements obtained in batch 1 or using method 1
 #' @param method2 measurements obtained in batch 2 or using method 2
 #' @param Ptype type of plot to be outputted c("scatter", "MAplot")
-#' @param metrics if return metrics is set to \code{TRUE}, returns Rc, Ca and R2
-#' @param xlabel label for x axis
-#' @param ylabel label for y axis
+#' @param metrics if \code{TRUE}, prints Rc, Ca, and R2 to console
+#' @param xlabel x-axis label for scatterplot
+#' @param ylabel y-axis label for scatterplot
 #' @param title title for the main plot
 #' @param subtitle subtitle of plot
 #' @param xrange range of x axis
@@ -30,23 +30,24 @@
 #' a6 <- a1 + rnorm(20, 0, 0.8)
 #' par(mfrow = c(3, 2), mar = c(5.1, 4.1, 1.5, 1.5))
 #'
-#' # Scatterplots
-#' CCplot(a1, a1, Ptype = "scatter", "X", "Y", "Perfect Agreement", subtitle = letters[1])
-#' CCplot(a1, a2, Ptype = "scatter", "X", "Y", "Very Good Agreement", subtitle = letters[2])
-#' CCplot(a1, a3, Ptype = "scatter", "X", "Y", "Location Shift", subtitle = letters[3])
-#' CCplot(a1, a4, Ptype = "scatter", "X", "Y", "Scale Shift", subtitle = letters[4])
-#' CCplot(a1, a5, Ptype = "scatter", "X", "Y", "Location and Scale Shift", subtitle = letters[5])
-#' CCplot(a1, a6, Ptype = "scatter", "X", "Y", "Measurement Error", subtitle = letters[6])
+#' m2 <- list(a1, a2, a3, a4, a5, a6)
+#' mains <- c("Perfect Agreement", "Very Good Agreement", "Location Shift",
+#'            "Scale Shift", "Location and Scale Shift", "Measurement Error")
+#' subs <- letters[1:6]
 #'
-#' # MAplots
-#' CCplot(a1, a1, Ptype = "MAplot", "X", "Y", "Perfect Agreement", subtitle = letters[1])
-#' CCplot(a1, a2, Ptype = "MAplot", "X", "Y", "Very Good Agreement", subtitle = letters[2])
-#' CCplot(a1, a3, Ptype = "MAplot", "X", "Y", "Location Shift", subtitle = letters[3])
-#' CCplot(a1, a4, Ptype = "MAplot", "X", "Y", "Scale Shift", subtitle = letters[4])
-#' CCplot(a1, a5, Ptype = "MAplot", "X", "Y", "Location and Scale Shift", subtitle = letters[5])
-#' CCplot(a1, a6, Ptype = "MAplot", "X", "Y", "Measurement Error", subtitle = letters[6])
+#' # Scatterplots
+#' mapply(function(y, t, s)
+#'   CCplot(method1 = a1, method2 = y, Ptype = "scatter",
+#'          xlabel = "X", ylabel = "Y", title = t, subtitle = s),
+#'   y = m2, t = mains, s = subs)
+#'
+#' # MAplots and show metrics
+#' mapply(function(y, t, s)
+#'   CCplot(method1 = a1, method2 = y, Ptype = "MAplot",
+#'          title = t, subtitle = s, metrics = TRUE),
+#'   y = m2, t = mains, s = subs)
 CCplot <- function(method1, method2, Ptype = "None", metrics = FALSE,
-                   xlabel = "", ylabel = "", title = "", subtitle = "",
+                   xlabel = "", ylabel = "", title = "", subtitle = NULL,
                    xrange = NULL, yrange = NULL, MArange = c(-3.5, 5.5)) {
   assertthat::assert_that(length(method1) == length(method2))
   tmp.ccc <- epiR::epi.ccc(method1, method2, ci = "z-transform",
@@ -61,14 +62,19 @@ CCplot <- function(method1, method2, Ptype = "None", metrics = FALSE,
   scl <- paste0("Scale shift:", round(tmp.ccc$s.shift, 2))
   z <- lm(method2 ~ method1)
   tmp.mean <- mean(tmp.ccc$blalt$delta)
-  tmp.sd <- sqrt(var(tmp.ccc$blalt$delta))
+  tmp.sd <- sd(tmp.ccc$blalt$delta)
+  if (!is.null(subtitle)) {
+    sub <- paste("(", subtitle, ")")
+  } else {
+    sub <- NULL
+  }
   if (is.null(xrange))
     xrange <- range(method1)
   if (is.null(yrange))
     yrange <- range(method2)
   if (Ptype == "scatter") {  # Scatter Plot
     plot(method1, method2, xlab = xlabel, xlim = xrange, ylim = yrange,
-         ylab = ylabel, pch = 16, sub = paste("(", subtitle, ")"))
+         ylab = ylabel, main = title, pch = 16, sub = sub)
     abline(a = 0, b = 1, lty = 2)
     abline(z, lty = 1)
     usr <- par("usr")  	# get user coordinates
@@ -80,7 +86,7 @@ CCplot <- function(method1, method2, Ptype = "None", metrics = FALSE,
     par(usr = usr)	# restore original user coordinates
   } else if (Ptype == "MAplot") {  # Bland-Altman or MAplot
     plot(tmp.ccc$blalt$mean, tmp.ccc$blalt$delta, pch = 16, xlab = "Average",
-         ylab = "Difference", sub = paste("(", subtitle, ")"), ylim = MArange)
+         ylab = "Difference", main = title, sub = sub, ylim = MArange)
     abline(h = tmp.mean, lty = 1, col = "gray")
     abline(h = tmp.mean - (2 * tmp.sd), lty = 2, col = "gray")
     abline(h = tmp.mean + (2 * tmp.sd), lty = 2, col = "gray")
@@ -92,7 +98,7 @@ CCplot <- function(method1, method2, Ptype = "None", metrics = FALSE,
     text(0.5, 0.81, cclab, adj = 0)
     par(usr = usr)	# restore original user coordinates
   }
-  if (metrics == TRUE) {
+  if (metrics) {
     return(c(Rc = round(tmp.ccc$rho.c[, 1], digits = 2),
              Ca = round(tmp.ccc$C.b, 2),
              R2 = round(cor(method1, method2), 2)))
