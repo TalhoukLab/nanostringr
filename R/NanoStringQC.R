d5d4061faaa66692557df9a97fb7e4b27193a13f
Derek Chiu <dchiu@bccrc.ca>
2017-02-03 16:31:51 -0800

    add unit tests to cover new assertions in NanoStringQC

--- R/NanoStringQC.R
+++ R/NanoStringQC.R
@@ -2,11 +2,19 @@
 #'
 #' Computes and returns NanoString quality control metrics and flags.
 #'
-#' @param raw matrix of raw counts obtained from nCounter (rows represent genes, columns represent samples).
-#' The first three columns must be labeled: \code{c("Code.Class", "Name", "Accession")} and contain that information.
-#' @param exp matrix of annotations with rows in the same order as the columns of \code{raw}. Requires a column labeled \code{"File.Name"} with entries corresponding to sample names in \code{raw}, also needs columns \code{c("fov.counted", "fov.count", "binding.density")}.These fields can be extracted from the nanostring RCC files.
-#' @param detect threshold of percentage of genes expressed over limit of detection (LOD) that we would like to detect (not decimal), defaults to 80 percent.
-#' @param sn signal to noise ratio of the housekeeping genes we are willing to tolerate, defaults to 150.
+#' @param raw matrix of raw counts obtained from nCounter (rows represent genes,
+#'   columns represent samples). The first three columns must be labeled:
+#'   \code{c("Code.Class", "Name", "Accession")} and contain that information.
+#' @param exp matrix of annotations with rows in the same order as the columns
+#'   of \code{raw}. Requires a column labeled \code{"File.Name"} with entries
+#'   corresponding to sample names in \code{raw}, also needs columns
+#'   \code{c("fov.counted", "fov.count", "binding.density")}.These fields can be
+#'   extracted from the nanostring RCC files.
+#' @param detect threshold of percentage of genes expressed over limit of
+#'   detection (LOD) that we would like to detect (not decimal), defaults to 80
+#'   percent.
+#' @param sn signal to noise ratio of the housekeeping genes we are willing to
+#'   tolerate, defaults to 150.
 #' @return matrix of annotations updated with normalization parameters
 #' @author Aline Talhouk, Derek Chiu
 #' @import dplyr
@@ -15,17 +23,18 @@
 #' exp.OVD <- subset(expQC, OVD == "Yes")
 #' expOVD <- NanoStringQC(ovd.r, exp.OVD)
 NanoStringQC <- function(raw, exp, detect = 80, sn = 150) {
-  
+
   # Run a bunch of checks to make sure the data is in the right order
   assertthat::assert_that(check_colnames(raw))  # Checks format of raw counts
   assertthat::assert_that(check_genes(raw))  # Checks that HK genes are specified
   assertthat::assert_that(ncol(raw) == nrow(exp) + 3)
-  if(all(grepl("[[:digit:]]",substring(colnames(raw[, -(1:3)]),1,1)))){
-  		assertthat::assert_that(all(substring(colnames(raw[, -(1:3)]), 2) == exp$File.Name))
-  	} else if (all(grepl("[[:alpha:]]",substring(colnames(raw[, -(1:3)]),1,1)))) {
-  		assertthat::assert_that(all(substring(colnames(raw[, -(1:3)]), 1) == exp$File.Name))
-  	}
-  	
+  cn <- colnames(raw[, -(1:3)])
+  if (all(grepl("[[:digit:]]", substring(cn, 1, 1)))) {
+    assertthat::assert_that(all(substring(cn, 2) == exp$File.Name))
+  } else if (all(grepl("[[:alpha:]]", substring(cn, 1, 1)))) {
+    assertthat::assert_that(all(substring(cn, 1) == exp$File.Name))
+  }
+
   sn.in <- sn
   genes <- raw$Name
   rownames(raw) <- genes
@@ -33,13 +42,15 @@ NanoStringQC <- function(raw, exp, detect = 80, sn = 150) {
   PCgenes <- genes[raw$Code.Class == "Positive"]
   NCgenes <- genes[raw$Code.Class == "Negative"]
   Hybgenes <- genes[raw$Code.Class != "Endogenous"]
-  if(!all(grepl("[[:digit:]]",PCgenes))) {stop("Positive controls must have concentrations in brackets: ex POS_A(128)")}
-PCconc <- as.numeric(sub("\\).*", "", sub(".*\\(", "", PCgenes)))
+  if (!all(grepl("[[:digit:]]", PCgenes))) {
+    stop("Positive controls must have concentrations in brackets: ex POS_A(128)")
+  }
+  PCconc <- as.numeric(sub("\\).*", "", sub(".*\\(", "", PCgenes)))
   flag.levs <- c("Failed", "Passed")
   . <- linPC <- linFlag <- fov.counted <- fov.count <- perFOV <- ncgMean <-
     ncgSD <- llod <- lod <- gd <- averageHK <- binding.density <- pergd <-
     spcFlag <- normFlag <- imagingFlag <- linFlag <- rn <- NULL
-  
+
   exp <- exp %>%
     mutate(rn = rownames(.),
            linPC = round(apply(raw[PCgenes, -(1:3)], 2,
--- tests/testthat/test-NanoStringQC.R
+++ tests/testthat/test-NanoStringQC.R
@@ -13,3 +13,14 @@ test_that("Always returns data frame", {
 test_that("ncol(raw) == nrow(exp) + 3", {
   expect_equal(ncol(hlo.r), nrow(expQC[expQC$geneRLF == "HL1", ]) + 3)
 })
+
+test_that("No errors if column names start with a number", {
+  colnames(hlo.r) <- gsub("^X", "",  colnames(hlo.r))
+  expect_error(NanoStringQC(hlo.r, expQC[expQC$geneRLF == "HL1", ]), NA)
+})
+
+test_that("Error if positive controls don't have concentrations in brackets", {
+  hlo.r$Name[hlo.r$Code.Class == "Positive"] <-
+    gsub("\\([0-9].*", "\\1", hlo.r$Name[hlo.r$Code.Class == "Positive"])
+  expect_error(NanoStringQC(hlo.r, expQC[expQC$geneRLF == "HL1", ]))
+})
