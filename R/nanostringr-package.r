7a4933cef2bf9e90d53f85f65c21075f26bda200
Derek Chiu <dchiu@bccrc.ca>
2017-12-13 12:02:03 -0800

    use new package doc convention

--- R/nanostringr-package.r
+++ R/nanostringr-package.r
@@ -1,12 +1,6 @@
-#' nanostringr: Quality Control and normalization for nanostringr platform
-#'
-#' The nanostringr package provides a novel set of tools used for quality control purposes
-#' in the NanoString platform. It includes batch adjustment using the ratio method thus far.
-#'
-#' @name nanostringr
+#' @keywords internal
 #' @importFrom dplyr "%>%"
-#' @docType package
-NULL
+"_PACKAGE"
 
 #' Expression QC data
 #'
