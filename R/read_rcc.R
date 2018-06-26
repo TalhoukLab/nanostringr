ba9e8e521c7e5acc13456c070bd4a3936669758a
Derek Chiu <dchiu@bccrc.ca>
2018-06-26 12:17:03 -0700

    fix param name in doc, add get_sample_name helper fun

--- R/read_rcc.R
+++ R/read_rcc.R
@@ -8,7 +8,7 @@
 #' information about Fov count, Fov counted, binding density, date, GeneRLF,
 #' SampleID, etc. Both count and attribute data are then merged across samples.
 #'
-#' @param dir directory path where RCC files exist
+#' @param path directory path where RCC files exist
 #' @param pattern file pattern for matching file extensions ".RCC" (or ".rcc")
 #'
 #' @return A tibble with columns "File.Name", "fov.count", "fov.counted",
@@ -34,8 +34,7 @@ read_rcc <- function(path = ".", pattern = "\\.RCC$") {
 #' @noRd
 parse_counts <- function(file) {
   rcc_file <- readr::read_lines(file)
-  sample_id <- grep("<Sample_Attributes>", rcc_file) + 1
-  sample_name <- make.names(strsplit(rcc_file[sample_id], ",")[[1]][2])
+  sample_name <- get_sample_name(rcc_file)
   cs_header <- grep("<Code_Summary>", rcc_file) + 1
   cs_last <- grep("</Code_Summary>", rcc_file) - 1
   rcc_parsed <-
@@ -50,8 +49,7 @@ parse_counts <- function(file) {
 #' @noRd
 parse_attributes <- function(file) {
   rcc_file <- readr::read_lines(file)
-  File.id <- grep("<Sample_Attributes>", rcc_file) + 1
-  File.Name <- make.names(strsplit(rcc_file[File.id], ",")[[1]][2])
+  File.Name <- get_sample_name(rcc_file)
   fov.count <- get_attr(rcc_file, "FovCount")
   fov.counted <- get_attr(rcc_file, "FovCounted")
   binding.density <- get_attr(rcc_file, "BindingDensity")
@@ -66,4 +64,11 @@ get_attr <- function(rcc_file, attr) {
     strsplit(split = ",") %>%
     purrr::pluck(1, 2) %>%
     as.numeric()
+}
+
+#' @param rcc_file RCC file
+#' @noRd
+get_sample_name <- function(rcc_file) {
+  id <- grep("<Sample_Attributes>", rcc_file) + 1
+  make.names(strsplit(rcc_file[id], ",")[[1]][2])
 }
