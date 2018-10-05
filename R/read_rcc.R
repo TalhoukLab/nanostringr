ae44eeb2ad4801fcae27fad5b57c7d655156760f
Derek Chiu <dchiu@bccrc.ca>
2018-10-05 10:55:15 -0700

    export parse_* funs and combine docs with read_rcc

--- R/read_rcc.R
+++ R/read_rcc.R
@@ -1,18 +1,24 @@
 #' Read NanoString RCC files
 #'
-#' Read a directory of RCC files and merge into count data and sample
-#' attributes.
+#' Read directory of RCC files and merge samples into count and attribute
+#' data.
 #'
 #' RCC files for a sample are direct outputs from NanoString runs. We can
-#' extract counts for each gene in a sample. Sample attributes include
-#' information about Fov count, Fov counted, binding density, date, GeneRLF,
-#' SampleID, etc. Both count and attribute data are then merged across samples.
+#' extract counts for each gene in a sample. Sample attributes include sample
+#' ID, GeneRLF, date, cartridge ID, lane number, Fov count, Fov counted, and
+#' binding density. Both count and attribute data are then merged across
+#' samples.
 #'
-#' @param path directory path where RCC files exist
-#' @param pattern file pattern for matching file extensions ".RCC" (or ".rcc")
+#' If `path` points to a zipped RCC file with multiple samples, the zip file is
+#' unzipped and a directory of RCC sample files are created with the same name.
+#'
+#' @param path directory path for multiple RCC files
+#' @param pattern pattern for matching file extensions ".RCC" or ".rcc"
 #'
 #' @return A tibble with columns "File.Name", "fov.count", "fov.counted",
 #'   "binding.density".
+#' @author Derek Chiu
+#' @name rcc
 #' @export
 read_rcc <- function(path = ".", pattern = "\\.RCC$") {
   if (!dir.exists(path)) {
@@ -32,8 +38,9 @@ read_rcc <- function(path = ".", pattern = "\\.RCC$") {
   tibble::lst(raw, exp)
 }
 
-#' @param file RCC file path
-#' @noRd
+#' @param file RCC file name
+#' @name rcc
+#' @export
 parse_counts <- function(file) {
   rcc_file <- readr::read_lines(file)
   sample_name <- get_attr(rcc_file, "^ID,.*[[:alpha:]]")
@@ -46,8 +53,9 @@ parse_counts <- function(file) {
     dplyr::rename(Code.Class = .data$CodeClass, !!sample_name := .data$Count)
 }
 
-#' @param file RCC file path
-#' @noRd
+#' @inheritParams parse_counts
+#' @name rcc
+#' @export
 parse_attributes <- function(file) {
   rcc_file <- readr::read_lines(file)
   attr_patterns <- c("^ID,.*[[:alpha:]]", "GeneRLF", "Date", "CartridgeID",
