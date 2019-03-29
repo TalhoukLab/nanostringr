38baa5c6e9a79148d3283d62daa25c25c06c0575
Derek Chiu <dchiu@bccrc.ca>
2019-03-29 15:18:10 -0700

    simplify vector and condition

--- R/HKnorm.R
+++ R/HKnorm.R
@@ -20,11 +20,11 @@
 HKnorm <- function(raw.data, is.logged = FALSE, corr = 0.0001) {
   assertthat::assert_that(check_colnames(raw.data))
   assertthat::assert_that(check_genes(raw.data))
-  rawdat <- raw.data[, -(1:3), drop = FALSE]
+  rawdat <- raw.data[, -1:-3, drop = FALSE]
   rownames(rawdat) <- raw.data$Name
   hks <- raw.data$Code.Class == "Housekeeping"
   refs <- raw.data$Code.Class != "Endogenous"
-  if (is.logged == FALSE) {
+  if (!is.logged) {
     rawdat <- rawdat + corr
     logHK <- apply(log2(rawdat[hks, , drop = FALSE]), 2, mean)
     logXpr <- log2(rawdat[!refs, , drop = FALSE])
